22237a5c4e1deb6db50b90f7858dc52c76780a2e
mattn <mattn.jp@gmail.com>
2011-05-18 17:02:37 +0900

    ...

--- eg/expr.pl
+++ eg/expr.pl
@@ -5,11 +5,5 @@ use warnings;
 use VimRemote;
 
 VimRemote::init();
-warn VimRemote::serverlist;
-#my @s = split /\n/, VimRemote::serverlist;
-#my @s = split /\n/, VimRemote::serverlist;
-#warn @s;
-#if (@s) {
-##warn VimRemote::expr($s[0], '1+2');
-#}
+warn VimRemote::expr(shift, '1+2');
 VimRemote::uninit();
